5ccb397ab9f01e3b23c587705e7c49a9911bc8b0
AshokWPD <ashokarun385@gmail.com>
2023-08-04 15:03:35 +0530

    design change

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -16,11 +16,13 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
 
   List<dynamic> users =[];
+   late bool loading;
+  var error;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text("hello")) ),
+      appBar: AppBar(title: const Center(child: Text("api call")) , backgroundColor: Colors.transparent,),
       body:ListView.builder(itemCount: users.length,itemBuilder: (context,index){
        final user = users[index];
        final email = user['user']['email'];
@@ -49,6 +51,8 @@ class _HomescreenState extends State<Homescreen> {
           },
         );
       }),
+      
+
       floatingActionButton: 
              FloatingActionButton(onPressed: fetchdata,backgroundColor: Colors.blue, child: const Icon(Icons.api_outlined),)
 
@@ -56,20 +60,55 @@ class _HomescreenState extends State<Homescreen> {
       
     
   }
+ 
 
   void fetchdata() async {
-  print('Fetching data');
+  setState(() {
+    loading = true; // Assuming you have a 'loading' state variable to track loading status
+    error = null;   // Assuming you have an 'error' state variable to track errors
+  });
+
   final url = Uri.parse('https://randomuser.me/api/0.8/?results=20');
 
- final responce =await http.get(url);
- final body = responce.body;
- final json = jsonDecode(body);
- setState(() {
-   users = json['results'];
- });
- 
-print("fetsch success!!!!!!!!!!!!!!!!!!");
+  try {
+    final response = await http.get(url);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+  } catch (e) {
+    setState(() {
+      error = "Error fetching data: $e"; // Update the error state with the error message
+    });
+  } finally {
+    setState(() {
+      loading = false; // Whether success or error, loading is finished
+    });
+  }
+
+  print("fetch success!!!!!!!!!!!!!!!!!!");
 }
+
+
+
+
+
+
+
+//   void fetchdata() async {
+    
+//   final url = Uri.parse('https://randomuser.me/api/0.8/?results=20');
+
+//  final responce =await http.get(url);
+//  final body = responce.body;
+//  final json = jsonDecode(body);
+//  setState(() {
+//    users = json['results'];
+//  });
+ 
+// print("fetsch success!!!!!!!!!!!!!!!!!!");
+// }
   
 }
 
